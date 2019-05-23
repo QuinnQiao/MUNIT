eb13e69c51ffda0a53d478933da17647eebff6cd
QuinnQiao <1500012919>
2019-05-23 09:51:42 +0800

    support 176*176

--- train.py
+++ train.py
@@ -2,7 +2,8 @@
 Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
-from utils import get_all_data_loaders, prepare_sub_folder, write_html, write_loss, get_config, write_2images, Timer
+from utils import prepare_sub_folder, write_html, write_loss, get_config, write_2images, Timer
+from utils import get_all_data_loaders, get_all_data_loaders_centercrop
 import argparse
 from torch.autograd import Variable
 from trainer import MUNIT_Trainer, UNIT_Trainer
@@ -23,6 +24,7 @@ parser.add_argument('--output_path', type=str, default='.', help="outputs path")
 parser.add_argument("--resume", action="store_true")
 parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
 parser.add_argument('--gpu_id', type=int, default=0, help='which gpu to use')
+parser.add_argument('--centercrop', action='store_true', default=False, help='if centercrop when load dataset')
 opts = parser.parse_args()
 
 cudnn.benchmark = True
@@ -42,7 +44,8 @@ elif opts.trainer == 'UNIT':
 else:
     sys.exit("Only support MUNIT|UNIT")
 trainer.cuda(device)
-train_loader_a, train_loader_b, test_loader_a, test_loader_b = get_all_data_loaders(config)
+get_loaders = get_all_data_loaders_centercrop if opts.centercrop else get_all_data_loaders
+train_loader_a, train_loader_b, test_loader_a, test_loader_b = get_loaders(config)
 train_display_images_a = torch.stack([train_loader_a.dataset[i] for i in range(display_size)]).cuda(device)
 train_display_images_b = torch.stack([train_loader_b.dataset[i] for i in range(display_size)]).cuda(device)
 test_display_images_a = torch.stack([test_loader_a.dataset[i] for i in range(display_size)]).cuda(device)
